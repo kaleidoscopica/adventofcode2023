efdb0c8654dc4b25c4c0ebf43d49780264c9afb8
kaleidoscopica <rayearth53@yahoo.com>
2023-12-05 16:08:56 -0600

    Day 1-2 in progress

--- Day1/1-2.py
+++ Day1/1-2.py
@@ -11,6 +11,25 @@ def main():
   for item in calibration_data:
     digits_list = []
 
-### Tbd, functions
+    # Create a list of tuples, each tuple mapping an index to the digit it represents (or begins, in the case of letter-spelled digits)
+    digits_list = find_digits(item, digits_list)
+    digits_list = find_ones(item, digits_list)
+
+
+# Returns a list of tuples of (index, digit) where a plain digit is found
+def find_digits(s, digits_list):
+  for index, character in enumerate(s):
+    if character.isdigit():
+      digits_list.append((index, character))
+  return digits_list
+
+# Returns a list of tuples of (index, "1") where the spelled letter "one" begins
+def find_ones(s, digits_list):
+  for index, character in enumerate(s):
+    if character == "o" and (index+2) < len(s):
+      if s[index+1] == "n":
+        if s[index+2] == "e":
+          digits_list.append((index, "1"))
+  return digits_list
 
 main()
