646656af175ee6f64e7ab46a2d7712effae5cc50
kaleidoscopica <rayearth53@yahoo.com>
2023-12-05 16:11:12 -0600

    Day 1-2 in progress

--- Day1/1-2.py
+++ Day1/1-2.py
@@ -15,6 +15,20 @@ def main():
     digits_list = find_digits(item, digits_list)
     digits_list = find_ones(item, digits_list)
 
+    # unzip the list of tuples to make one list of indices, and another list of digits
+    unzipped_digits_list = [[i for i, j in digits_list], [j for i, j in digits_list]]
+
+    # get the min index to find the first digit, and the max index to find the second digit
+    min_index = find_minimum(unzipped_digits_list[0])
+    num1 = unzipped_digits_list[1][min_index]
+    max_index = find_maximum(unzipped_digits_list[0])
+    num2 = unzipped_digits_list[1][max_index]
+
+    # num1 and num2 are still strings, cast them to int and add them to the sum
+    number = int(num1 + num2)
+    sum += number
+  
+  print("The sum of all the calibration values:", sum)
 
 # Returns a list of tuples of (index, digit) where a plain digit is found
 def find_digits(s, digits_list):
