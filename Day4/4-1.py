c45d16fd8dbd868d5f852bb96edca8f65b88e9ea
kaleidoscopica <rayearth53@yahoo.com>
2023-12-07 19:04:19 -0600

    Day 4-1 complete

--- Day4/4-1.py
+++ Day4/4-1.py
@@ -13,7 +13,31 @@ def main():
       scratchcard_data.append(new_line)
   file.close()
 
-  for line in scratchcard_data:
-    print(line)
+  point_total = 0
+
+  # Iterate over each scratchcard in the scratchcard data
+  for scratchcard in scratchcard_data:
+    points = 0
+    winning_numbers = []
+
+    print("Examining scratchcard:", scratchcard)
+    # See if any number on the scratchcard is also found in the winning numbers
+    for number in scratchcard[1]:
+      if number in scratchcard[0]:
+        winning_numbers.append(number)
+    print("This card's winning numbers are:", winning_numbers)
+
+    # Point calculation
+    # The first winning match is worth one point, and each match after that doubles the points
+    if len(winning_numbers) == 1:
+      points = 1
+    elif len(winning_numbers) > 1:
+      points = 2**(len(winning_numbers)-1)
+    print("Adding", points, "points to the total!")
+
+    # Add the points to the point total
+    point_total += points
+
+  print("The total points of these scratchcards is:", point_total)
 
 main()
